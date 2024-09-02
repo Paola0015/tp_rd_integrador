6201f7d5c14886a03436707ba6a0fdaed8a20d96
Maximiliano Kildoff <maxi.kildoff@gmail.com>
2024-09-02 18:06:56 -0300

    Configuracion de conexion mysql

---
+++ src/configure/dbConfig.js
@@ -0,0 +1,15 @@
+//Configuración de conexión a sql local tener en cuenta el user y pass del sql local
+
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '123',
+  database: 'idw',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = connection;
