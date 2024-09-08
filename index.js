a8c937791285cff69d041628c3c0e73d1e1dc39e
Maximiliano Kildoff <maxi.kildoff@gmail.com>
2024-09-08 18:02:02 -0300

    Configuracion de variables de entorno y nodemailer a terminnar

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 //modulos para importar agregar en el package.json "type": "module"
 import express from 'express';
+import dotenv from 'dotenv'; //para utilizar variables de entorno
+import nodemailer from 'nodemailer';
 
+dotenv.config(); //configra las variables de entorno desde .env
+
+const PORT = process.env.PUERTO;
 const app = express();
-const PORT = 3000;
 
 //Midlewares
 
-//EndPoints
+//Rutas
 app.get('/', (req, res) => {
-    res.send('Server OK!')
+    res.status(200).json({'Estado':true })
   })
- 
+
 //Confiración del servidor express
 app.listen(PORT, () => {
     console.log(`*** Servidor escuchando en puerto: ${PORT} ***`)
