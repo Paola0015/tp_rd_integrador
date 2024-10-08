25287a26988bbe77133824ea600cae3fd46bffca
Maximiliano Kildoff <maxi.kildoff@gmail.com>
2024-10-08 19:49:02 -0300

    Crud de usuarios

---
+++ src/database/usuarios.js
@@ -0,0 +1,29 @@
+import { conexion } from "./conexion";
+
+export default class Usuarios{
+
+    buscarTodos = async () => {
+        const sql = 'SELECT * FROM usuarios;';
+        const [result] = await conexion.query(sql);
+        return result;
+    }
+
+    buscarActivos = async () => {
+        const sql = 'SELECT * FROM usuarios WHERE activo = 1;';
+        const [result] = await conexion.query(sql);
+        return result;
+    }
+
+    buscarInactivos = async () => {
+        const sql = 'SELECT * FROM usuarios WHERE activo = 0;';
+        const [result] = await conexion.query(sql);
+        return result;
+    }
+    
+    buscarPorId = async (idUsuario) => {
+        const sql = `SELECT * FROM usuarios WHERE activo = 1 AND idUsuario = ?`;
+        const [result] = await conexion.query(sql, [idUsuario]);
+        return (result.length > 0) ? result[0] : null;
+    }
+    
+}
