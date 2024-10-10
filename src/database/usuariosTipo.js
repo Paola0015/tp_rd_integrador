cd9c3adf0f45f558cfd6c3db51435468147be4a3
Maximiliano Kildoff <maxi.kildoff@gmail.com>
2024-10-10 18:31:40 -0300

    CRUD tipos usuarios

--- src/database/usuariosTipo.js
+++ src/database/usuariosTipo.js
@@ -7,6 +7,10 @@ export default class UsuariosTipo{
         const [result] = await conexion.query(sql);
         return result;
     };
-
+    buscarPorId = async (idUsuarioTipo) => {
+        const sql = `SELECT * FROM usuariosTipo  WHERE activo = 1 AND idUsuario = ?`;
+        const [result] = await conexion.query(sql, [idUsuario]);
+        return (result.length > 0) ? result[0] : null;
+    }
     
 }
