bf93fc8397240c097bcbe01a1224ebed53fd141a
Maximiliano Kildoff <maxi.kildoff@gmail.com>
2024-10-10 19:45:49 -0300

    CRUD usuariosTipo terminado

--- src/services/usuariosTipoService.js
+++ src/services/usuariosTipoService.js
@@ -7,8 +7,21 @@ export default class UsuariosTipoService{
     }
     buscarTodos = () =>{
         return this.usuariosTipo.buscarTodos();
-    
     }
 
-    
+    buscarPorId = (idUsuarioTipo) => {
+        return this.usuariosTipo.buscarPorId(idUsuarioTipo);
+    }
+
+    actualizarUsuariosTipo = (idUsuarioTipo, datosActualizados) => {
+        return this.usuariosTipo.actualizarUsuariosTipo(idUsuarioTipo,datosActualizados);
+    }
+
+    crearUsuarioTipo = (nuevoUsuarioTipo) =>{
+        return this.usuariosTipo.crearUsuarioTipo(nuevoUsuarioTipo);
+    }
+
+    borrarUsuarioTipo = (idUsuarioTipo) =>{
+        return this.usuariosTipo.borrarUsuarioTipo(idUsuarioTipo);
+    }
 }
--- src/v1/routes/usuariosTipoRoutes.js
+++ src/v1/routes/usuariosTipoRoutes.js
@@ -5,7 +5,10 @@ const router = express.Router();
 const usuariosTipoController = new UsuariosTipoController();
 
 router.get('/', usuariosTipoController.buscarTodos);
-
+router.get('/:id', usuariosTipoController.buscarPoId);
+router.patch('/:id',usuariosTipoController.actualizarUsuariosTipo)
+router.post('/', usuariosTipoController.crearUsuarioTipo)
+router.delete('/:id', usuariosTipoController.borrarUsuarioTipo)
 
 export {router};
 
