f3630282c26d618e06ee648c9f732ed0061894ff
Paola <paoandre225@gmail.com>
2024-10-10 20:36:27 -0300

    Mantenimiento de datos específicos en src/index.js

--- src/index.js
+++ src/index.js
@@ -10,20 +10,14 @@ import { fileURLToPath } from "url";
 import validateContentType from "./middlewares/validateContentType.js";
 
 // rutas
-<<<<<<< HEAD
 import { router as v1ReclamosEstadoRouter } from "./v1/routes/reclamosEstadosRoutes.js";
 import { router as v1ReclamosRouter } from "./v1/routes/reclamosRoutes.js";
-import { router as v1Usuarios } from "./v1/routes/usuariosRoutes.js";
-import { authRouter } from "./v1/routes/authRoutes.js";
-import { router as v1OficinasRouter } from "./v1/routes/oficinasRoutes.js"; // Asegúrate de agregar esto
-=======
-import { router as  v1ReclamosEstadoRouter } from './v1/routes/reclamosEstadosRoutes.js';
-import { router as v1ReclamosRouter } from './v1/routes/reclamosRoutes.js';
-import { router as v1Usuarios } from './v1/routes/usuariosRoutes.js';
-import {router as v1UsuariosTipo} from './v1/routes/usuariosTipoRoutes.js'
->>>>>>> main
 
-dotenv.config(); //configura las variables de entorno
+import { router as v1Usuarios } from "./v1/routes/usuariosRoutes.js";
+
+import { authRouter } from "./v1/routes/authRoutes.js";
+
+dotenv.config(); // configura las variables de entorno
 const PORT = process.env.PUERTO;
 
 const app = express();
@@ -34,21 +28,15 @@ app.use(validateContentType);
 app.get("/", (req, res) => {
   res.json({ estado: true });
 });
-//Rutas de los EndPoints
-<<<<<<< HEAD
+
+// Rutas de los EndPoints
 app.use("/v1/reclamos-estados", v1ReclamosEstadoRouter);
 app.use("/v1/reclamos", v1ReclamosRouter);
 app.use("/v1/usuarios", v1Usuarios);
 app.use("/v1/auth", authRouter);
-app.use("/v1/oficinas", v1OficinasRouter);
-=======
-app.use('/v1/reclamos-estados', v1ReclamosEstadoRouter);
-app.use('/v1/reclamos', v1ReclamosRouter);
-app.use('/v1/usuarios',v1Usuarios);
-app.use('/v1/usuariosTipo', v1UsuariosTipo);
->>>>>>> main
+// Mantén solo las rutas necesarias
 
-//Config del servidor express
+// Config del servidor express
 app.listen(PORT, () => {
   console.log(`*** Servidor escuchando en puerto: ${PORT} ***`);
 });
