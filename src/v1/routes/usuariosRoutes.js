604f5901f3dd83b46dcea379395a2e6d1dbce253
Paola <paoandre225@gmail.com>
2024-11-10 12:38:05 -0300

    subiendo cambios en mi rama

--- src/v1/routes/usuariosRoutes.js
+++ src/v1/routes/usuariosRoutes.js
@@ -6,35 +6,6 @@ import { verifyToken, verifyRole } from "../../middlewares/auth.js"; // Importar
 const router = express.Router();
 const usuariosController = new UsuariosController();
 
-// Ruta para obtener usuarios con paginación
-router.get("/", verifyToken, verifyRole(["admin"]), async (req, res) => {
-  const pagina = parseInt(req.query.pagina) || 1; // Página solicitada por el usuario
-  const porPagina = parseInt(req.query.porPagina) || 10; // Número de usuarios por página
-
-  try {
-    const usuarios = await usuariosController.buscarConPaginacion(
-      pagina,
-      porPagina
-    );
-    res.json(usuarios); // Devolvemos los usuarios en formato JSON
-  } catch (error) {
-    res.status(500).json({ error: "Error al obtener usuarios" });
-  }
-});
-
-router.get(
-  "/:id",
-  verifyToken,
-  verifyRole(["admin"]),
-  usuariosController.buscarPorId
-);
-import express from "express";
-import UsuariosController from "../../controllers/usuariosController.js";
-import { verifyToken, verifyRole } from "../../middlewares/auth.js"; // Importar middleware
-
-const router = express.Router();
-const usuariosController = new UsuariosController();
-
 // Rutas de usuarios protegidas por autenticación y roles
 router.get(
   "/",
